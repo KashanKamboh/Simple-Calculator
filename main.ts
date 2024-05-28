a4e488f5a8647afdf34cee0fbc1c5f24112ed7dd
kashankamboh <kashankamboh12@gmail.com>
2024-05-28 21:26:42 +0500

    Adding a Simple CLI Calculator Project

---
+++ main.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgBlueBright.bold("\n\tWellCome To 'Code With Kashaf' - Calculator\n" ));
+
+
+let answer = await inquirer.prompt([
+    {
+        name: "firstnumber",
+        type: "number",
+        message: "Enter first number :"
+},
+        {
+        name: "secondnumber",
+        type: "number",
+        message: "Enter second number :"
+ },
+    {
+        message: "Select One Operator to Perform Operations :",
+        type : "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+
+if (answer.operator === "Addition"){
+    console.log(chalk.greenBright(answer.firstnumber + answer.secondnumber));
+}
+
+else if (answer.operator === "Subtraction"){
+    console.log(chalk.greenBright(answer.firstnumber - answer.secondnumber));
+}
+else if (answer.operator === "Multiplication"){
+    console.log(chalk.greenBright(answer.firstnumber * answer.secondnumber));
+}
+else if (answer.operator === "Division"){
+    console.log(chalk.greenBright(answer.firstnumber / answer.secondnumber));
+}
+else{
+ console.log(chalk.redBright("Invalid Number"));
+}
